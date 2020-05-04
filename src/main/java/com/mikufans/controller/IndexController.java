77fd2b7a0c6ed4daf2b7de308dbc8605c97eb3c7
Mikufans <1448672486@qq.com>
2020-05-04 22:53:13 +0800

    commit

--- src/main/java/com/mikufans/controller/IndexController.java
+++ src/main/java/com/mikufans/controller/IndexController.java
@@ -1,16 +1,19 @@
 package com.mikufans.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mikufans.mvc.annotation.Controller;
 import org.mikufans.mvc.annotation.Request;
 import org.mikufans.mvc.bean.View;
 
 @Controller
+@Slf4j
 public class IndexController
 {
     @Request.Get("/index")
     public View index()
     {
-        return new View("index.jsp").data("test","test");
+        log.info("indexController.......");
+        return new View("index.jsp").data("test", "test");
     }
 
 }
